11b470e7b254ac54b64470e846156346c6f970e5
Nicocchi <nicocchilover@gmail.com>
2020-05-16 00:26:49 -0400

    added minor details

--- src/App.js
+++ src/App.js
@@ -36,35 +36,6 @@ const randomGrid = () => {
     return rows;
 };
 
-// const simulate = (g) => {
-//     let gridCopy = g.map((k) => k);
-//     for (let i = 0; i < numRows; i++) {
-//         for (let j = 0; j < numCols; j++) {
-//             let neighbors = 0;
-//             operations.forEach(([x, y]) => {
-//                 const newI = i + x;
-//                 const newJ = j + y;
-//                 if (
-//                     newI >= 0 &&
-//                     newI < numRows &&
-//                     newJ >= 0 &&
-//                     newJ < numCols
-//                 ) {
-//                     neighbors += g[newI][newJ];
-//                 }
-//             });
-
-//             if (neighbors < 2 || neighbors > 3) {
-//                 gridCopy[i][j] = 0;
-//             } else if (g[i][j] === 0 && neighbors === 3) {
-//                 gridCopy[i][j] = 1;
-//             }
-//         }
-//     }
-
-//     return gridCopy;
-// }
-
 function App() {
     const [grid, setGrid] = useState(() => {
         return generateEmptyGrid();
@@ -83,7 +54,6 @@ function App() {
         }
 
         // Simulate
-        // setGrid((g) => simulate(g));
         setGrid((g) => {
             return produce(g, (gridCopy) => {
                 for (let i = 0; i < numRows; i++) {
@@ -156,6 +126,7 @@ function App() {
                     Toggle Grid
                 </button>
             </div>
+            <div className="memory"><h2>In memory of John Horton Conway - December 26, 1937 - April 11, 2020</h2></div>
 
             <div className="game-area">
                 <div
